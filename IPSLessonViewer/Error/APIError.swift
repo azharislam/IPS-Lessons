abcd24de63d2885c1fa04f2466bc8ebdb01b8b81
Azhar Islam <azhartislam@gmail.com>
2022-04-13 17:49:32 +0100

    Add API error handling class

---
+++ IPSLessonViewer/Error/APIError.swift
@@ -0,0 +1,27 @@
+//
+//  APIError.swift
+//  IPSLessonViewer
+//
+//  Created by Azhar Islam on 13/04/2022.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from service"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown:
+            return "The error is unknown"
+        }
+    }
+}
